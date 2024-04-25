07883ab86f6c24a7282ff282583d64fb39618044
thiagomelop <thiagomelop@yahoo.com.br>
2024-04-24 21:30:54 -0300

    quase terminado

--- src/script.js
+++ src/script.js
@@ -1,15 +1,17 @@
-$(document).ready(function() {
-$(window).scroll(function() {
-    var secaoInicioPosicao = $("#inicio").offset().top;
-    var janelaPosicao = $(this).scrollTop();
+document.addEventListener("DOMContentLoaded", function() {
+    $(window).scroll(function() {
+        var secaoInicioPosicao = $("header #inicio").offset().top;
+        var janelaPosicao = $(this).scrollTop();
 
-    if (janelaPosicao >= secaoInicioPosicao) {
-    $(".menu a.oculto").fadeIn(500); /* Exibe o link "Início" quando passa da seção inicial */
-    } else {
-    $(".menu a.oculto").fadeOut(500); /* Oculta o link "Início" ao voltar para cima da seção inicial */
-    }
+        if (janelaPosicao >= secaoInicioPosicao) {
+            $(".menu a.oculto").fadeIn(500); /* Exibe o link "Início" quando passa da seção inicial */
+        } else {
+            $(".menu a.oculto").fadeOut(500); /* Oculta o link "Início" ao voltar para cima da seção inicial */
+        }
+    });
 });
-});
+
+
 
 
 const whatsappButton = document.getElementById('whatsappButton');
